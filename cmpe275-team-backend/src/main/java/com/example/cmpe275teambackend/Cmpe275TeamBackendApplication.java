4a02dc5705e6e71c878266504c7d4c5242da60d5
Qing Huang <hq1992518@msn.cn>
2017-12-08 16:54:25 -0800

    Init backend package

---
+++ cmpe275-team-backend/src/main/java/com/example/cmpe275teambackend/Cmpe275TeamBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.cmpe275teambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cmpe275TeamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cmpe275TeamBackendApplication.class, args);
+	}
+}
